00e4942283d45986a38e512ff3ba0a1de84e1ea5
Wahab Muhammad <wahabmuhammad95@gmail.com>
2025-08-19 18:27:13 +0700

    update job kirim pesan fonnte

--- app/Jobs/kirimPesanFonnte.php
+++ app/Jobs/kirimPesanFonnte.php
@@ -3,23 +3,27 @@
 namespace App\Jobs;
 
 use App\services\fonnteServices;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class kirimPesanFonnte implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $phone, $pesan;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(public $phone, public $pesan)
+    public function __construct($phone, $pesan)
     {
-        //
+        $this->phone = $phone;
+        $this->pesan = $pesan;
     }
 
     /**
@@ -28,6 +32,5 @@ class kirimPesanFonnte implements ShouldQueue
     public function handle(): void
     {
         fonnteServices::sendText($this->phone, $this->pesan);
-        sleep(2);
     }
 }
